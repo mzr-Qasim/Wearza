955800f7d129fffb126c0ce8a73f7a6464f6b07d
Muhammad Qasim <malikqasim20051@gmail.com>
2025-06-17 01:23:15 +0500

    commit

--- src/styleshowcase.js
+++ src/styleshowcase.js
@@ -1,8 +1,8 @@
 
     export const showCaseCard = [
-        { "id": 1, "title": "Take Your Style to the Next Level!", "caption": "Discover Your Style Inspiration Here!", "video": "https://res.cloudinary.com/dqhzrovwe/video/upload/v1750010968/sg9c0z12l3bxs4tzvnlv.mp4" },
+        { "id": 1, "title": "Take Your Style to the Next Level!", "caption": "Discover Your Style Inspiration Here!", "video": "https://res.cloudinary.com/dqhzrovwe/video/upload/v1750101963/19791932-hd_1080_1920_30fps_vc92pq.mp4" },
 
-        { "id": 2, "title": "Fashion Forward Finds", "caption": "Explore the Newest Trends for Women!",  "video": "https://res.cloudinary.com/dqhzrovwe/video/upload/v1750012320/un5fsptqgqfyusohly1z.mp4"},
+        { "id": 2, "title": "Fashion Forward Finds", "caption": "Explore the Newest Trends for Women!",  "video": "https://res.cloudinary.com/dqhzrovwe/video/upload/v1750101171/19867506-hd_1080_1920_30fps_mf2pny.mp4"},
 
 
 
