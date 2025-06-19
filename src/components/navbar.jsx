21ff533c77b118bd90b75fe36e91c06d32df893c
Muhammad Qasim <malikqasim20051@gmail.com>
2025-06-19 23:14:42 +0500

    commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ import { useContext } from "react"
 
 function Navbar() {
 
-    const {count, setCount} = useContext(countContext)
+    const { count, setCount } = useContext(countContext)
 
     const [showSearchbar, setShowSearchBar] = useState(false)
     const handleSearchBar = () => {
@@ -36,6 +36,7 @@ function Navbar() {
                             <li><a href="">blog</a></li>
                         </ul>
                     </div>
+
                     <a href="" className="main_logo"><img src={main_logo} alt="" /></a>
                     <div className="nav-right sm-hide">
                         <ul>
@@ -46,7 +47,29 @@ function Navbar() {
                         </ul>
 
                     </div>
-                    <button className="nav_hambtn" onClick={handleNavToggle}><i className={showMenu ? "icon-ham_close_btn" : "icon-hamburger"}></i></button>
+
+                    {/* <button className="nav_hambtn" onClick={handleNavToggle}><i className={showMenu ? "icon-ham_close_btn" : "icon-hamburger"}></i></button> */}
+                    <div className="navbar-offcanvas">
+                        <button className="nav_hambtn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" ><i className={showMenu ? "icon-ham_close_btn" : "icon-hamburger"}></i></button>
+
+                        <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                            <div className="offcanvas-header">
+                                <button className="menu_close" data-bs-dismiss="offcanvas"  aria-label="Close"><i className="icon-ham_close_btn"></i></button>
+                            </div>
+                            <div className="offcanvas-body">
+                                <div className="nav-left-md">
+                                    <ul>
+                                        <li><Link to={"/"}>home</Link></li>
+                                        <li><Link to={"shop"}>shop</Link></li>
+                                        <li><a href="">features</a></li>
+                                        <li><a href="">lookbook</a></li>
+                                        <li><Link to={"about-us"}>about us</Link></li>
+                                        <li><a href="">blog</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className={`searchMenu ${showSearchbar ? "searchbarshow" : ""}`}>
--- src/pages/product-detail.jsx
+++ src/pages/product-detail.jsx
@@ -89,14 +89,14 @@ function ProductDetail() {
                         </div>
                     </div>
                     <div className="productinfo">
-                        <ul class="nav nav-tabs" id="myTab" role="tablist">
-                            <li class="nav-item" role="presentation">
+                        <ul className="nav nav-tabs" id="myTab" role="tablist">
+                            <li className="nav-item" role="presentation">
                                 <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">PRODUCT DETAILS</button>
                             </li>
-                            <li class="nav-item" role="presentation">
+                            <li className="nav-item" role="presentation">
                                 <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">REVIEWS</button>
                             </li>
-                            <li class="nav-item" role="presentation">
+                            <li className="nav-item" role="presentation">
                                 <button className="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">SHIPPING & RETURNS</button>
                             </li>
                         </ul>
