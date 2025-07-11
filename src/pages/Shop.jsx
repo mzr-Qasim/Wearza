019dc21385d72b5dfaa1cd717c1ba14e9115ef26
Muhammad Qasim <malikqasim20051@gmail.com>
2025-07-10 23:36:57 -0700

    commit

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -11,7 +11,7 @@ function Shop() {
     return (
         <>
             <div className="title-bar">
-                <div className="container custom-container-lg">
+                <div className="container custom-container-lg"> 
                     <div className="title-bar-inner">
                         <Link className="title-bar-link" to={"/"}>Home</Link>
                         <span>Collection</span>
